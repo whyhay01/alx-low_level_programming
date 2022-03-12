912fd6c4593015569be44192cf84e929450df1dd
whyhay01 <hyaaquub@gmail.com>
2022-03-11 17:39:10 -0800

    uodate

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,9 +3,6 @@
 #include <time.h>
 
 #include <stdio.h>
-
-
-
 /**
  * main - prints the last digit of the random
  *
@@ -13,9 +10,7 @@
  *
  * Return: Always 0 (Success)
  */  
-
 int main(void)
-
 {
 	int n;
 	
@@ -35,5 +30,4 @@ int main(void)
 		printf("and is less than 6 and not 0\n");
 	}							
 	return (0);
-
 }
