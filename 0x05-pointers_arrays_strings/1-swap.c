e0366f37a7bd73fb190bb1f0b79f9f06f22b5590
whyhay01 <hyaaquub@gmail.com>
2022-03-21 19:00:35 -0700

    Commit for task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * swap_int - Swap the the value of two int value
+ * @a: first int value
+ * @b: Second int value
+ *
+ * Return: Nothing
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,4 @@
 #include <unistd.h>
 
 void reset_to_98(int *n);
+void swap_int(int *a, int *b);
