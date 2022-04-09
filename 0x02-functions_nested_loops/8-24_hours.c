7e131cf923f0040fa8f205e26c398f19add4466b
Bello Yakub <hyaaquub@gmail.com>
2022-04-09 11:57:49 +0000

    fix style

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,11 +16,11 @@ void jack_bauer(void)
 
 		while (j < 60)
 		{
-			_putchar((i/10) + '0');
-			_putchar((i%10) + '0');
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
 			_putchar(':');
-			_putchar((j/10) + '0');
-			_putchar((j%10) + '0');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
 			_putchar('\n');
 			
 			j++;
