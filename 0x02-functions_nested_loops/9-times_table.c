e5b4df09f2a71cb14c67cb512d0cb809640d7dd3
Bello Yakub <hyaaquub@gmail.com>
2022-04-09 17:58:08 +0000

    fixed style

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,7 +5,7 @@
  */
 void times_table(void)
 {
-	int i,j,k;
+	int i , j , k;
 
 	for (i = 0; i < 10; i++)
 	{
@@ -23,7 +23,7 @@ void times_table(void)
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(k + '0');
-			}else if (k >= 10)
+			} else if (k >= 10)
 			{
 				_putchar(',');
 				_putchar(' ');
