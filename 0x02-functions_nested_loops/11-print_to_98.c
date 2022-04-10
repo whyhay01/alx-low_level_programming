8a7cccd1fae57715d12afd7ced42ddc0791cc5cb
Bello Yakub <hyaaquub@gmail.com>
2022-04-10 11:43:54 +0000

    betty style fixed

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,7 +13,7 @@ void print_to_98(int n)
 	{
 		for (i = n; i >= 98; i--)
 		{
-			if(i != 98)
+			if (i != 98)
 			{
 				printf("%d, ", i);
 			}
@@ -27,14 +27,14 @@ void print_to_98(int n)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			if(i != 98)
+			if (i != 98)
 			{
-				printf("%d, ",i);
+				printf("%d, ", i);
 			}
 
 			if (i == 98)
 			{
-				printf("%d",i);
+				printf("%d", i);
 			}
 		}
 	}
