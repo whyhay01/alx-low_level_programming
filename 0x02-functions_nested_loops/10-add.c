9ce1fda3c11131d12f485748a8dc47215806f4e8
Bello Yakub <hyaaquub@gmail.com>
2022-04-10 04:22:08 +0000

    fixed doc style issue

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,8 +1,9 @@
 #include "main.h"
-/*
+/**
  * add- adds two numbers and return the result
  * @a: first argument
  * @b: second argument
+ *
  * Return: always return the result of the operation
  */
 int add(int a, int b)
