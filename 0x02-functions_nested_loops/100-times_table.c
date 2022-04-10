ec21cb55fbde5338a150437c5234820b0a9fcf44
Bello Yakub <hyaaquub@gmail.com>
2022-04-10 12:48:03 +0000

    fixed multiplication error

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,13 +12,13 @@ void print_times_table(int n)
 
 	if (n >= 0 && n <= 15)
 	{
-		for ( i = 0; i <= n; i++)
+		for (i = 0; i <= n; i++)
 		{
 			for (j = 0; j <= n; j++)
 			{
 				l = j * i;
 
-				if ( j == 0)
+				if (j == 0)
 				{
 					_putchar(l + '0');
 				} else if (l < 10 && j != 0)
@@ -35,7 +35,7 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar((l / 10) + '0');
 					_putchar((l % 10) + '0');
-				}else if (l >= 100)
+				} else if (l >= 100)
 				{
 					_putchar(',');
 					_putchar(' ');
