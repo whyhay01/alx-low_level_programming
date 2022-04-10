6b8321a34e3812c7b523747ccb63261bc4cafa3b
Bello Yakub <hyaaquub@gmail.com>
2022-04-10 11:34:11 +0000

    fixed comma error

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,7 +15,7 @@ void print_to_98(int n)
 		{
 			if(i != 98)
 			{
-				printf("%d ,", i);
+				printf("%d, ", i);
 			}
 
 			if (i == 98)
@@ -29,7 +29,7 @@ void print_to_98(int n)
 		{
 			if(i != 98)
 			{
-				printf("%d ,",i);
+				printf("%d, ",i);
 			}
 
 			if (i == 98)
